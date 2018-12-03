acbf70119699ab8fa9b046b934ee1ec0809761f4
milin26 <milinpatel26@yahoo.com>
2018-12-03 18:34:16 +0530

    Tutorial : 3 module parameter passing and get notification when variable value gets changed

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Description :
 Steps to compile :
 1.Run				make
 2.Insert module			sudo insmod hello.ko
-3.See the added module		cat /proc/modules | grep hello
+3.See the added module		cat /proc/modules | grep hello, lsmod(See the list of module)
 4.See the logs in dmesg 	dmesg | tail -10
   kernel ring buffer	
 5.Remove module			sudo rmmod hello
@@ -22,5 +22,21 @@ Keypoints to remember:
 3.module_init();
 4.module_exit();
 
+Macros for module parameters:
+1.module_param();
+2.module_param_array();
+3.module_param_cb();
+
+structure where we will register event handler:
+const struct kernel_param_ops my_param_ops = 
+{
+        .set = &notify_param, // Use our setter ...[int(*set)(const char *val, struct kernem_param *kp)]
+        .get = &param_get_int, // .. and standard getter[int(*get)(char *buffer, const struct kernel_param *kp )]
+	.free = //[void(*free)(void *args)]
+};
+
+
 Note : 
 	Make sure before you run "make" command be super user using "sudo -i".
+	param_set_int(val, kp); // Use helper for write variable
+
--- hello_world.c
+++ hello_world.c
@@ -1,9 +1,11 @@
 /************************************************************************************************
-@ileName : simple_driver.c
-@author	 : sayli waykar 
-@teamLead: Rajesh Dommaraju 
-@etails  : It is a simple HelloWorld driver to load a module in the kernel  
-@icense  : SpanIdea Systems Pvt. Ltd. All rights reserved.
+FileName : simple_driver.c
+Author	 : MILIN PATEL 
+TeamLead: Rajesh Dommaraju 
+Details  : It is a simple HelloWorld driver to load a module in the kernel, 
+	   passing the parameters, update parameter from command line, 
+	   get notification message as well for it.  
+License  : SpanIdea Systems Pvt. Ltd. All rights reserved.
 ************************************************************************************************/
 
 /*******************************************************************************
@@ -11,15 +13,14 @@
 *******************************************************************************/
 
 #include <linux/module.h>
-
 /*******************************************************************************
 			 LOCAL MACROS		
 *******************************************************************************/
 
 #define U_MODULE_LICENSE	"GPL"
 #define U_MODULE_AUTHOR		"MILIN@SPANIDEA"
-#define U_MODULE_DESCRIPTION	"INITIAL HELLO WORLD MODULE"
-#define U_MODULE_VERSION	"0:1.0"
+#define U_MODULE_DESCRIPTION	"MODULE PARAMETERS PASSING"
+#define U_MODULE_VERSION	"0:1.1"
 
 /*******************************************************************************
 			 LOCAL TYPEDEFS		
@@ -36,9 +37,45 @@
 /*******************************************************************************
 			LOCAL FUNCTIONS		
 *******************************************************************************/
+/*---------------------------Module parameters--------------------------------*/
+int valueETX, arr_valueETX[4];//1, 2
+char *nameETX;//3
+int cb_valueETX = 0;//4
 
-static int hello_init(void);
-static void hello_exit(void);
+module_param(valueETX, int, S_IWUSR |S_IRUSR);//1
+module_param_array(arr_valueETX, int, NULL, S_IWUSR |S_IRUSR);//2
+module_param(nameETX, charp, S_IWUSR |S_IRUSR);//3
+
+/*---------------------------Module_param_cb()--------------------------------*/
+int notify_param(const char *val, const struct kernel_param *kp)
+{
+        int res = param_set_int(val, kp); // Use helper for write variable
+        if(res==0)
+	{
+		printk(KERN_INFO "val = %s", val);
+                printk(KERN_INFO "Call back function called...\n");
+                printk(KERN_INFO "New value of cb_valueETX = %d\n", cb_valueETX);
+                return 0;
+        }
+	else
+	{
+		return -1;
+	
+	}
+}
+const struct kernel_param_ops my_param_ops = 
+{
+	.set = &notify_param, // Use our setter ...[int(*set)(const char *val, const struct kernel_param *kp)]
+        .get = &param_get_int, // .. and standard getter[int(*get)(char *buffer, const struct kernel_param *kp)]
+				//[void(*free)(void *arg)]
+};
+module_param_cb(cb_valueETX, &my_param_ops, &cb_valueETX,S_IWUSR |S_IRUSR);//4
+/*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
+
+
+static int sample_module_init(void);
+static void sample_module_exit(void);
 
 /**********************************************************************************************
 function	 : sample_module_init
@@ -50,7 +87,15 @@ return param     : signed integer
 
 static int sample_module_init(void)
 {
-	printk(KERN_INFO "Initialize init module: %s ,%d\n", __func__, __LINE__);
+	int i=0;
+	printk(KERN_INFO "ValueETX = %d  \n", valueETX);
+        printk(KERN_INFO "cb_valueETX = %d  \n", cb_valueETX);
+        printk(KERN_INFO "NameETX = %s \n", nameETX);
+        for (i = 0; i < (sizeof arr_valueETX / sizeof (arr_valueETX[0])); i++)
+	{
+                printk(KERN_INFO "Arr_value[%d] = %d\n", i, arr_valueETX[i]);
+        }
+	printk(KERN_INFO "Initialize init module Success at %s ,%d\n", __func__, __LINE__);
 	return 0;
 }
 
@@ -64,7 +109,7 @@ return param     : void
 
 static void sample_module_exit(void)
 {
-	printk(KERN_INFO "Exiting exit module : %s ,%d\n", __func__, __LINE__);
+	printk(KERN_INFO "Exiting exit module success at %s ,%d\n", __func__, __LINE__);
 	return;
 }
 
